9eb2ae98f38f2c8de1acbd4045fbe5a4fb65d9f2
Kwon_jingu <wlsrndmswls1@naver.com>
2021-03-16 22:55:44 +0900

    3/16 react_2

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,21 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name : '',
+        nickname: ''
+    });
 
+    const nameInput = useRef();
+
+    const {name,nickname} = inputs;
+    
     const onChange = (e) => {
-        setText(e.target.value);
+        const {value, name} =e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
     };
 
     const onReset =() => {
-        setText('');
+        setInputs({
+            name: '',
+            nickname:'',
+        });
+        nameInput.current.focus();
     };
     return(
         <div>
-            <input onChange={onChange} value = {text} />
+            <input 
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+                ref={nameInput}
+            />
+
+            <input 
+                name="nickname"
+                placeholder={onChange}
+                value={nickname}      
+            />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값 : {text}</b>
+                <b>값 : </b>
+                {name} ({nickname})
             </div>
         </div>
     );
